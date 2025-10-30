016161b7d07e6fca45cb53debcd6d743f5a37878
cristianquerolalves1 <cristian.personal@mail.com>
2025-10-30 19:58:20 +0100

    quinto commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
@@ -7,7 +8,11 @@ export default {
         white: "#FFFFFF",
         black: "#161925",
         primary: "#1D4ED8",
-        secondary: "#0C8346",
+        secondary: "#1D4ED8",
+        bgLight: "#FFFFFF",
+        bgDark: "#161925",
+        textLight: "#161925",
+        textDark: "#FFFFFF",
       },
     },
   },
